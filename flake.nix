02a8453b3291144143a7b18ae38dd5c6854f60fa
adisbladis <adisbladis@gmail.com>
2026-02-10 16:01:51 +1300

    flake: Remove flake-utils
    This avoids a dependency which isn't necessary and also avoids [re-importing nixpkgs](https://zimbatm.com/notes/1000-instances-of-nixpkgs/).

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -14,50 +13,54 @@
     {
       self,
       nixpkgs,
-      flake-utils,
       rust-overlay,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
 
-        # Nightly toolchain with rust-src for build-std (size-optimized builds)
-        rustToolchain = pkgs.rust-bin.nightly.latest.default.override {
-          targets = [
-            "x86_64-unknown-linux-musl"
-            "aarch64-unknown-linux-musl"
-            "x86_64-apple-darwin"
-            "aarch64-apple-darwin"
-          ];
-          extensions = [ "rust-src" ];
-        };
+          # Nightly toolchain with rust-src for build-std (size-optimized builds)
+          rustToolchain = rust-overlay.packages.${system}.rust-nightly.override {
+            targets = [
+              "x86_64-unknown-linux-musl"
+              "aarch64-unknown-linux-musl"
+              "x86_64-apple-darwin"
+              "aarch64-apple-darwin"
+            ];
+            extensions = [ "rust-src" ];
+          };
 
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            rustToolchain
-            pkgs.pkg-config
-            pkgs.pkgsCross.musl64.stdenv.cc
-            pkgs.pkgsCross.aarch64-multiplatform-musl.stdenv.cc
-            pkgs.rust-analyzer
-          ];
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              rustToolchain
+              pkgs.pkg-config
+              pkgs.pkgsCross.musl64.stdenv.cc
+              pkgs.pkgsCross.aarch64-multiplatform-musl.stdenv.cc
+              pkgs.rust-analyzer
+            ];
 
-          shellHook = ''
-            echo "TRAMP-RPC development shell (nightly + build-std)"
-            echo ""
-            echo "Build:"
-            echo "  ./scripts/build-all.sh                         # x86_64 Linux (static musl)"
-            echo "  ./scripts/build-all.sh aarch64-unknown-linux-musl"
-            echo "  ./scripts/build-all.sh x86_64-apple-darwin"
-            echo "  ./scripts/build-all.sh --all"
+            shellHook = ''
+              echo "TRAMP-RPC development shell (nightly + build-std)"
+              echo ""
+              echo "Build:"
+              echo "  ./scripts/build-all.sh                         # x86_64 Linux (static musl)"
+              echo "  ./scripts/build-all.sh aarch64-unknown-linux-musl"
+              echo "  ./scripts/build-all.sh x86_64-apple-darwin"
+              echo "  ./scripts/build-all.sh --all"
 
-            export CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER="${pkgs.pkgsCross.musl64.stdenv.cc}/bin/x86_64-unknown-linux-musl-gcc"
-            export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER="${pkgs.pkgsCross.aarch64-multiplatform-musl.stdenv.cc}/bin/aarch64-unknown-linux-musl-gcc"
-          '';
-        };
-      }
-    );
+              export CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER="${pkgs.pkgsCross.musl64.stdenv.cc}/bin/x86_64-unknown-linux-musl-gcc"
+              export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER="${pkgs.pkgsCross.aarch64-multiplatform-musl.stdenv.cc}/bin/aarch64-unknown-linux-musl-gcc"
+            '';
+          };
+        }
+      );
+    };
 }
