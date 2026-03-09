315eb4f9baa5a95508e1c5ae565e0b044c04d4e4
Troy Hinckley <t.macman@gmail.com>
2026-03-09 13:03:31 -0500

    Properly handle symlink directories in completions

--- test/tramp-rpc-tests.el
+++ test/tramp-rpc-tests.el
@@ -1076,6 +1076,19 @@ and returns a valid path."
       (should (member "file-aab.txt" completions))
       (should-not (member "other.txt" completions)))))
 
+(ert-deftest tramp-rpc-test12b-file-name-completion-symlink-directory ()
+  "Ensure completion marks symlinks to directories with trailing slash."
+  (skip-unless (tramp-rpc-test-enabled))
+
+  (tramp-rpc-test--with-temp-dir dir
+    (let ((real-dir (concat dir "/real-dir"))
+          (link-dir (concat dir "/link-dir")))
+      (make-directory real-dir t)
+      (make-symbolic-link "real-dir" link-dir)
+      (let ((completions (file-name-all-completions "" dir)))
+        (should (member "real-dir/" completions))
+        (should (member "link-dir/" completions))))))
+
 ;;; ============================================================================
 ;;; Test 13: File System Info
 ;;; ============================================================================
